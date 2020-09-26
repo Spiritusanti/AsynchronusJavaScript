652e301167ea10c50064e57c08bb8c1c3a639853
Jacob McCracken <mccrackenjacob@gmail.com>
2020-09-26 18:51:13 +0000

    Added async/await section

--- index.js
+++ index.js
@@ -52,4 +52,120 @@ console.log('3')
 // setTimeout tells our call stack to trigger the API and place ('2') in the callback queue. The stack then moves on to the next operations and then after two seconds the ('2') gets added back to callback queue. The event loop, which is constantly checking the stack also looks at the callback queue and when it see something adds it into the call stack to be run.
 
 
+// Promises sections:
+// A promise is an object that may produce a single value sometime in the future. Either a resolved value or a reason it's not resolved (rejected). 3 possible states: fulfilled, rejected, or pending
 
+// promise example:
+
+movePlayer(100, 'Left')
+.then(() => movePlayer(400, 'Left'))
+.then(() => movePlayer(10, 'Right'))
+.then(() => movePlayer(350, 'Left'))
+
+// how do we create a promise?
+const promise = new Promise((resolve, reject) => {
+  if (true){
+  resolve('Stuff worked');
+  } else {
+  reject('It broke....');
+  }
+})
+
+promise.then(result => console.log(result))
+
+// So what can we do with promises?
+promise.then(result => result + '!').then(result2 => {
+  throw error
+  console.log(result2)
+})
+.catch(() => console.log('error!'))
+//catch statement checks if anything before it fails. important to put it at or near the end of the statement.
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'HII')
+})
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'pookie')
+})
+
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "is it me you're looking for?")
+})
+
+Promise.all([promise, promise2, promise3, promise4]).then(values => {
+  console.log(values);
+})
+
+
+// Time for a real world example:
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+Promise.all(urls.map(url => {
+  return fetch(url).then(resp => resp.json())
+})).then(results => {
+  console.log(results[0])
+  console.log(results[1])
+  console.log(results[2])
+}).catch(()=> console.log('error'))
+
+
+// Async / Await
+
+// part of ES8 and built on top of promises. Under the hood it is a function that returns a promise. Benefit is that it makes code easier to read.
+
+// example function:
+
+async function playerStart() {
+  const firstMove = await moveplayer(100, 'Left'); //pause
+  await movePlayer(12, 'Right'); //pause
+}
+
+//async await is syntactic sugar --> same promise operation under the hood but makes it easier to read.
+
+fetch('https://jsonplaceholder.typicode.com/users').then(resp => resp.json()).then(console.log)
+
+async function fetchUsers() {
+ const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+ const data = await resp.json();
+ console.log(data);
+}
+
+
+// another example
+
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+Promise.all(urls.map(url => {
+  return fetch(url).then(resp => resp.json())
+})).then(results => {
+  console.log(results[0])
+  console.log(results[1])
+  console.log(results[2])
+}).catch(()=> console.log('error'))
+
+// how do we catch errors with async await?
+// use a try / catch block.
+const getData = async function() {
+  try {
+    const [ users, posts, albums ] = await Promise.all(urls.map(url => {
+    return fetch(url).then(resp => resp.json())
+    }
+  ))
+  console.log('users', users)
+  console.log('users', posts)
+  console.log('users', albums)
+  } catch {
+    console.log('oops')
+  }
+}
+
+// ES9
